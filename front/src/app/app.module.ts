eb95f3376ce9de333a8205132c18d3f38a348c8e
SeiryuJP <seiryujp7@gmail.com>
2023-01-31 17:01:05 +0100

    tarjetas creadas

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { OfertasAlumnoComponent } from './ofertas-alumno/ofertas-alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    OfertasAlumnoComponent
   ],
   imports: [
     BrowserModule
---
+++ front/src/app/ofertas-alumno/ofertas-alumno.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertas-alumno',
+  templateUrl: './ofertas-alumno.component.html',
+  styleUrls: ['./ofertas-alumno.component.scss']
+})
+export class OfertasAlumnoComponent {
+
+}
