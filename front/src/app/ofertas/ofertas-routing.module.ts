2cc933f0b52f656c84ebec0e413db28dd9844341
SeiryuJP <seiryujp7@gmail.com>
2023-02-01 20:23:10 +0100

    componente ofertas-empresa

--- front/src/app/ofertas/ofertas-routing.module.ts
+++ front/src/app/ofertas/ofertas-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OfertasAlumnoComponent } from './ofertas-alumno/ofertas-alumno.component';
+import { OfertasEmpresaComponent } from './ofertas-empresa/ofertas-empresa.component';
 
 const routes: Routes = [
   {
     path: '',
     component: OfertasAlumnoComponent
+  },
+  {
+    path: 'testing',
+    component: OfertasEmpresaComponent
   }
 ];
 
--- front/src/app/ofertas/ofertas.module.ts
+++ front/src/app/ofertas/ofertas.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OfertasAlumnoComponent } from './ofertas-alumno/ofertas-alumno.component';
 import { OfertasRoutingModule } from './ofertas-routing.module';
+import { OfertasEmpresaComponent } from './ofertas-empresa/ofertas-empresa.component';
 
 @NgModule({
   declarations: [
-    OfertasAlumnoComponent
+    OfertasAlumnoComponent,
+    OfertasEmpresaComponent
   ],
   exports: [
-    OfertasAlumnoComponent
+    OfertasAlumnoComponent,
+    OfertasEmpresaComponent
   ],
   imports: [
     CommonModule,
