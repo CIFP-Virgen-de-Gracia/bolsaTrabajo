f60a82f32f554c43b03d25cb80e1e6ff54c47b8b
ManuelHergueta <manuelherguetaharo@gmail.com>
2023-02-03 09:28:38 +0100

    Preparado para mergear con develop

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmpresasModule } from './empresas/empresas.module';
@@ -12,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     EmpresasModule,
     SharedModule
