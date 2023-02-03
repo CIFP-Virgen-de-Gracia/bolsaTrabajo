2a89bd419a97deef0d316c42d99aa90e22fac518
InesRM <107609883+InesRM@users.noreply.github.com>
2023-02-03 17:43:19 +0100

    interfaces, cambio de carpeta por error

--- front/src/auth/login-successful/login-successful.component.ts
+++ front/src/auth/login-successful/login-successful.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IRegisterUser } from '../../../iregister';
+import { IRegisterUser } from '../interfaces/iregister';
 
 
 @Component({
