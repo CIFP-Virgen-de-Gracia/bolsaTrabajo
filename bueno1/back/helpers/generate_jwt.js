dfbb144a3b73bd103f3ab3f1f6fa91907787e3b0
InesRM <107609883+InesRM@users.noreply.github.com>
2023-02-03 21:17:05 +0100

    borrar

--- bueno1/back/helpers/generate_jwt.js
+++ bueno1/back/helpers/generate_jwt.js
@@ -1,20 +1,4 @@
 const jwt = require('jsonwebtoken')
-// const authconfig = require('../config/auth');
-
-// const generarJWT = (uid = '') => {
-    
-//     //En el token podemos hacer que viaje (en el payload) el id de ese usuario. No supone un gran fallo de seguridad y nos permite sacar la información del mismo en los middleware.
-//     console.log("UID:" + uid)
-//     let token = jwt.sign({ uid }, authconfig.SECRETORPRIVATEKEY, {
-//         expiresIn: '24h' // 24 hours
-//       });
-//       console.log("Token:" + token)
-//     return token;
-// }
-
-// module.exports ={
-//     generarJWT
-// }
 
 
 const generarJWT = (uid = '') => {
