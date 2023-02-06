33001de127ac222dabc044edfe163d711201a658
MiriamOliver <miriamoliver99@gmail.com>
2023-02-06 22:09:40 +0100

    organizacion y correcion de estructura

--- back/controllers/Conexion/ConexionEmpresa.js
+++ back/controllers/Conexion/ConexionEmpresa.js
@@ -1,8 +1,4 @@
 const Empresa = require('../../models/Empresa');
-const CicloAlumno = require('../../models/CicloAlumno');
-const Ciclo = require('../../models/Ciclo');
-const Alumno = require('../../models/Alumno');
-const User = require("../../models/User");
 const ConexionSequelize = require('./ConexionSequelize');
 
 class ConexionEmpresa extends ConexionSequelize {
@@ -18,41 +14,6 @@ class ConexionEmpresa extends ConexionSequelize {
     resultado = await Empresa.findAll();
     this.desconectar();
     return resultado;
-  }
-
-  getAlumno = async(nif) => {
-      let resultado = {};
-      let ciclos = {};
-      let idCiclos = [];
-      let u = await User.findByPk(nif);
-      let a = await Alumno.findByPk(nif);
-      let c = await CicloAlumno.findAll({ 
-          attributes: ['id_ciclos'],
-          where: { nif_alumno: nif } });
-      c.forEach(ciclo => idCiclos.push(ciclo.dataValues.id_ciclos));
-      let ciclo = await this.getCiclosAlumno(idCiclos); 
-      resultado = concatenate.jsonConcat(resultado, u.dataValues);
-      resultado = concatenate.jsonConcat(resultado, a.dataValues);
-      ciclo.forEach(cicl => ciclos[cicl.dataValues.sigla] = (cicl.dataValues));
-      resultado['ciclos'] = ciclos;
-      if (!resultado){
-          throw error;
-      }
-      return resultado;
-  }
-
-
-  getCiclosAlumno = async(idCiclo) => {
-      let ciclo = {};
-      ciclo = await Ciclo.findAll({
-              attributes: ['sigla','nombre','fecha'],
-              where: {
-                id: {
-                  [Op.in]: idCiclo
-                }
-              }
-          });
-      return ciclo;
   }
   
     getEmpresa = async(id) => {
