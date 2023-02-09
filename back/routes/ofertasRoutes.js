44f3386beef59ed3df9407d334d93725e5a83763
SeiryuJP <seiryujp7@gmail.com>
2023-02-09 14:09:44 +0100

    validacion ruta post oferta

--- back/routes/ofertasRoutes.js
+++ back/routes/ofertasRoutes.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
 const controlador = require('../controllers/ofertasController');
 const router = Router();
+const { check } = require('express-validator');
 
 router.get('/', controlador.verListadoOfertas);
 router.get('/:id', controlador.verOferta);
-router.post('/crear', controlador.crearOferta);
+router.post('/crear',
+    [
+        check('titulo').not().isEmpty(),
+        check('descripcion').not().isEmpty(),
+        check('lugar').not().isEmpty(),
+        check('presencial').not().isEmpty(),
+        check('jornada').not().isEmpty(),
+    ]
+    ,controlador.crearOferta);
 router.delete('/:id', controlador.eliminarOferta);
 router.put('/modificar/:id', controlador.actualizarOferta);
 
