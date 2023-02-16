aaacc637eb5238c7c1b83028dbacdf95e48aebd0
InesRM <107609883+InesRM@users.noreply.github.com>
2023-02-17 00:58:03 +0100

    back google, upload img

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -141,11 +141,90 @@ const logout = (req, res = response) => {
   res.status(200).json({ msg: "Logout correcto." });
 };
 
+const renewToken = async (req, res = response) => {
+  const { nif, nick, email, password, status, rol } = req.body;
+  const token = await generarJWT(
+    nif,
+    nick,
+    email,
+    password,
+    status,
+    rol
+  );
+  res.json({ token });
+};
+
+const googleSignin = async(req, res = response) => {
+
+  const { id_token } = req.body;
+
+  //Para probar inicialmente que recibimos el token del front:
+  // res.status(200).json({msg:"Token recibido",id_token});
+  
+  //Una vez comprobado que el token se recibe desde el front, instalamos: npm install google-auth-library --save 
+  //Y ya podemos usar el código siguiente para verificar el token.
+  try {
+      const { nif, nick, email, password, avatar } = await googleVerify( id_token );
+      //Si seguimos y no salta la excepción estarmos verificados por Google.
+
+      let usuario = await User.findOne({ email });
+      console.log(`Comprobaríamos el usuario: ${email}, ${nick}`);
+      //Verificar si existe el usuario. Además de la verificación del token de Google, vemos si existe en nuestra BD, caso de no existir lo creamos.
+      const conx = new Conexion();
+      u = conx.getUsuarioRegistrado(nif, password)    
+          .then( usu => {
+              console.log('Usuario correcto!  ' + usu[0].nif);
+              const token = generarJWT(usu[0].nif)
+              console.log(usu)
+              console.log(token);
+              res.status(200).json({usu, token});
+          })
+          .catch( err => {
+              console.log('Usuario incorrecto!  ' + err);
+              res.status(500).json({msg: 'Usuario incorrecto!'});
+          });
+
+      if ( !u ) {
+          // Tengo que crearlo
+          const data = {
+              nif,
+              nick,
+              email,
+              password: ':P',
+              avatar: img,
+              google: true
+          };
+          //Crearíamos el usario con Mongoose o con MySQL, según lo que usemos.
+          usuario = new User( data );
+          console.log(`Creando el usuario ${data}`)
+          await usuario.save();        
+      }
+
+
+      const token = generarJWT(email); //Si entramos, podemos generar nuestro propio token de seguridad con el correo del usuario. Ese token será el que usemos para el uso de la API.
+      res.status(200).json({email, token});
+      
+  } catch (error) {
+
+      res.status(400).json({
+          msg: 'Token de Google no es válido'
+      })
+
+  }
+
+
+
+}
+
+
+
 module.exports = {
   login,
   register,
   logout,
   registerAdmin,
-  registerEmpresa
+  registerEmpresa,
+  renewToken,
+  googleSignin
 
 };
---
+++ back/helpers/google.verify.js
@@ -0,0 +1,29 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client( process.env.GOOGLE_CLIENT_ID );
+
+const googleVerify = async( idToken = '' ) => {
+
+  const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+
+  console.log(ticket); //Aquí podemos ver el payload que nos devuelve Google con la información del usuario.
+  
+  const { name: nombre, 
+          picture: img, 
+          email: correo
+        } = ticket.getPayload();
+  
+  return { nombre, img, correo };
+
+}
+
+
+
+module.exports = {
+    googleVerify
+}
